9d291997a14f16c6eb5c9e65d82490b1f6032ae8
TdoGG21 <36172162+TdoGG21@users.noreply.github.com>
2018-03-20 21:17:17 -0700

    Initial commit

---
+++ app/src/main/java/com/studdybuddy/finalstudybuddy/DiscussionBoard.java
@@ -0,0 +1,55 @@
+package com.studdybuddy.finalstudybuddy;
+
+/**
+ * Created by Tony on 3/16/2018.
+ */
+
+public class DiscussionBoard {
+
+    private String title, desc, imageUrl, username;
+
+    public DiscussionBoard(String title, String desc, String imageUrl, String username) {
+        this.title = title;
+        this.desc = desc;
+        this.imageUrl=imageUrl;
+        this.username = username;
+    }
+
+    public DiscussionBoard() {
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+}
+
+
+
